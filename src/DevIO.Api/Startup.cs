156d313abd6493c3ceb8380d98b598174599297c
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-30 17:45:39 -0300

    feat: add extension method to resolve dependencies of MeuDbContext and IFornecedorRespository

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using DevIO.Api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace DevIO.Api
             services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
