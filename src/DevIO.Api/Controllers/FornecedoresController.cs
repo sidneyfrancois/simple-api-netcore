b631b0a49e2bc900c81cf0025d32dd20fb1838e0
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-30 20:15:53 -0300

    feat:add Excluir controller method to remove Fornecedor entity from the database

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -71,9 +71,29 @@ namespace DevIO.Api.Controllers
             return Ok(fornecedor);
         }
 
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEnedereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            var result = await _fornecedorService.Remover(id);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+
         public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
         {
             return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorEnedereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
         }
     }
 }
