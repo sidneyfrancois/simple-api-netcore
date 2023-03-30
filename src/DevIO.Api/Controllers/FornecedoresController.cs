1b652dadca121c3d1af8b0f4a92c2c7c299bcb60
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-30 17:31:15 -0300

    feat: move FornecedoresController to new file, separation

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,28 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedor);
+        }
+    }
+}
--- src/DevIO.Api/Controllers/MainController.cs
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -13,22 +13,4 @@ namespace DevIO.Api.Controllers
     {
        
     }
-
-    [Route("api/fornecedores")]
-    public class FornecedoresController : MainController
-    {
-        private readonly IFornecedorRepository _fornecedorRepository;
-
-        public FornecedoresController(IFornecedorRepository fornecedorRepository)
-        {
-            _fornecedorRepository = fornecedorRepository;
-        }
-
-        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
-        {
-            var fornecedor = await _fornecedorRepository.ObterTodos();
-
-            return Ok(fornecedor);
-        }
-    }
 }
