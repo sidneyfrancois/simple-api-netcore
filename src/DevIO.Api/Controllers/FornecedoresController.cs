5b952da7d5eab6610bbb009228b1d2d26bb7af17
Sidney François <sidney.fvm.dev@outlook.com>
2023-03-30 17:32:21 -0300

    feat: add IMapper as dependency injection

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -1,4 +1,5 @@
-﻿using DevIO.Api.ViewModels;
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,10 +13,12 @@ namespace DevIO.Api.Controllers
     public class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
 
-        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
         {
             _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
         }
 
         public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
