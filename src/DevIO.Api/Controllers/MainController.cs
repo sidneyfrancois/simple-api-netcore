a41d77bd814ff0993db84b574a871fdc68ac3e73
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-01 22:45:11 -0300

    feat: add custom response in case of success or error

--- src/DevIO.Api/Controllers/MainController.cs
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -1,6 +1,8 @@
 ﻿using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,55 @@ namespace DevIO.Api.Controllers
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-       
+        private readonly INotificador _notificador;
+
+        public MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(n => n.Mensagem)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalido(modelState);
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected void NotificarErroModelInvalido(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                var errorMsg = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+                NotificarErro(errorMsg);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
     }
 }
