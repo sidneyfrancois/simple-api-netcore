8607cda89f5f9fa34ea4f26aaac3a220e4d89286
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-02 10:06:21 -0300

    feat: add repository, service and mapper as dependency

--- src/DevIO.Api/Controllers/ProdutosController.cs
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using DevIO.Business.Intefaces;
+﻿using AutoMapper;
+using DevIO.Business.Intefaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,18 @@ namespace DevIO.Api.Controllers
 {
     public class ProdutosController : MainController
     {
-        public ProdutosController(INotificador notificador) : base (notificador)
-        {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
 
+        public ProdutosController(INotificador notificador,
+                                  IMapper mapper,
+                                  IProdutoRepository produtoRepository, 
+                                  IProdutoService produtoService) : base (notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
         }
     }
 }
