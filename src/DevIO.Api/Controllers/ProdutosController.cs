c34fe800010e8d5397bb6e96945acb5abd9aa582
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-02 10:03:17 -0300

    feat: add ProdutosController and INotificador as dependency injection

---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Business.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    public class ProdutosController : MainController
+    {
+        public ProdutosController(INotificador notificador) : base (notificador)
+        {
+
+        }
+    }
+}
